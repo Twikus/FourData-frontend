f4c4b3e2c972bce68fe72e6eb11f6099e882d0c6
Axel D <axel@spliiit.com>
2024-08-17 16:13:33 +0200

    setup: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import mkcert from 'vite-plugin-mkcert'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    mkcert({
+      hosts: ['localhost']
+    })
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  server: {
+    port: 5173,
+    host: '127.0.0.1',
+  },
 })
