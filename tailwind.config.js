ad0d06af10f71196aae5e8ce010ec40f639b6aee
Axel D <axel@spliiit.com>
2024-08-18 13:39:06 +0200

    feat/dashboard : add blue primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,9 @@ module.exports = {
         fontFamily: {
           sans: ['Inter var', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
         },
+        colors: {
+          "primary-color": "var(--blue-primary)",
+        },
       },
     },
     plugins: [],
