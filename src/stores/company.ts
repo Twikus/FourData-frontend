c7d4b7512e52865a354d99cf2691b5491cda292a
Axel D <axel@spliiit.com>
2024-08-21 22:52:30 +0200

    feat/companies: update create and add update endpoint in store

--- src/stores/company.ts
+++ src/stores/company.ts
@@ -12,9 +12,9 @@ export const useCompanyStore = defineStore('company', {
     getters: {},
     actions: {
         async fetchCompanies() {
-            try {
-                const token = getToken();
+            const token = getToken();
 
+            try {
                 const response = await axiosClient.get('companies', {
                     headers: {
                         Authorization: `Bearer ${token}`,
@@ -35,10 +35,22 @@ export const useCompanyStore = defineStore('company', {
                 displayError(error);
             }
         },
-        async createCompany(params: Siren | Siret) {
-            try {
-                const token = getToken();
+        async createCompany(siren?: number, siret?: number) {
+            let params = {};
+            
+            const token = getToken();
 
+            if (siren) {
+                params = {
+                    siren: siren,
+                };
+            } else {
+                params = {
+                    siret: siret,
+                };
+            }
+
+            try {
                 const response = await axiosClient.post('companies', params, {
                     headers: {
                         Authorization: `Bearer ${token}`,
@@ -46,10 +58,37 @@ export const useCompanyStore = defineStore('company', {
                 });
 
                 response.data.status = getStatus(response.data.status);
-
                 this.companies.unshift(response.data);
 
                 addEvent('create-company-success');
+            } catch (error: any) {
+                displayError(error);
+
+                if (error.response.status === 400 && error.response.data.error === 'Company already exists') {
+                    addEvent('create-company-already-exists');
+                } else if (error.response.status === 400 && error.response.data.error === 'Company not found') {
+                    addEvent('create-company-not-found');
+                }
+            }
+        },
+        async updateCompany(params: Company) {
+            params.status = params.status === 'Ouverte' ? true : false;
+
+            try {
+                const token = getToken();
+
+                const response = await axiosClient.put(`companies/${params.id}`, params, {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                response.data.status = getStatus(response.data.status);
+
+                const index = this.companies.findIndex((company) => company.id === response.data.id);
+                this.companies[index] = response.data;
+
+                addEvent('update-company-success');
             } catch (error) {
                 displayError(error);
             }
