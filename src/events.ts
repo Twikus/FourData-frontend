8f5077ba1af6f27827759bd0fd50f4719a4cb9a2
Axel D <axel@spliiit.com>
2024-08-21 22:51:52 +0200

    feat/companies: add events

--- src/events.ts
+++ src/events.ts
@@ -35,10 +35,37 @@ export function registerGlobalEvents() {
         });
     });
 
+    window.addEventListener('create-company-already-exists', () => {
+        toast.add({
+        severity: 'error',
+        summary: 'L\'entreprise existe déjà',
+        life: 3000,
+        group: 'br'
+        });
+    });
+
+    window.addEventListener('create-company-not-found', () => {
+        toast.add({
+        severity: 'error',
+        summary: 'L\'entreprise n\'existe pas',
+        life: 3000,
+        group: 'br'
+        });
+    });
+
     window.addEventListener('remove-company-success', () => {
         toast.add({
         severity: 'success',
         summary: 'Entreprise supprimée',
+        life: 3000,
+        group: 'br'
+        });
+    });
+
+    window.addEventListener('update-company-success', () => {
+        toast.add({
+        severity: 'success',
+        summary: 'Entreprise mise à jour',
         life: 3000,
         group: 'br'
         });
