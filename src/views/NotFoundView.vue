288943574f1006c956dcc63197167af7217726a4
Axel D <axel@spliiit.com>
2024-08-22 09:53:57 +0200

    feat/register: update style of 404

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex flex-col items-center justify-center bg-gray-100">
-      <h1 class="text-6xl font-bold text-gray-800 mb-4">404</h1>
-      <p class="text-2xl text-gray-600 mb-8">Page non trouvée</p>
-    </div>
+  <div class="flex flex-col items-center justify-center bg-gray-100 w-full">
+    <h1 class="text-6xl font-bold text-gray-800 mb-4">404</h1>
+    <p class="text-2xl text-gray-600 mb-8">Page non trouvée</p>
+  </div>
 </template>
